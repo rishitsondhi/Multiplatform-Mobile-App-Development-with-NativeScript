c6cc32f9cc2dd1bd48ce80d92a7fbec4003a6f66
Zeeshan Haider <zeeshanhaider87@gmail.com>
2017-11-19 17:20:40 +0500

    Side Drawer

--- Assignment 1/conFusion/app/app.module.ts
+++ Assignment 1/conFusion/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { AppRoutingModule } from "./app.routing";
+import { NativeScriptUISideDrawerModule } from "nativescript-telerik-ui/sidedrawer/angular";
+
 import { AppComponent } from "./app.component";
 
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DrawerComponent } from "./shared/drawer/drawer.component";
 
 import { DishService } from './services/dish.service'; 
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
@@ -25,12 +28,14 @@ import { baseURL } from './shared/baseurl';
     imports: [
         NativeScriptModule,
         AppRoutingModule,
-        NativeScriptHttpModule
+        NativeScriptHttpModule,
+        NativeScriptUISideDrawerModule        
     ],
     declarations: [
         AppComponent,
         MenuComponent,
-        DishdetailComponent
+        DishdetailComponent,
+        DrawerComponent
     ],
     providers: [
         {provide: 'BaseURL', useValue: baseURL}, 
---
+++ Assignment 1/conFusion/app/shared/drawer/drawer.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({ 
+    selector: 'drawer-content', 
+    templateUrl: './shared/drawer/drawer.component.html'
+ })
+export class DrawerComponent {
+    constructor() { }
+}
