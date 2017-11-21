6a30c573d44c2c962c44bff042e2e5e5a070d7d9
Zeeshan Haider <zeeshanhaider87@gmail.com>
2017-11-21 22:32:58 +0500

    Advanced Lists

--- Assignment 2/conFusion/app/app.module.ts
+++ Assignment 2/conFusion/app/app.module.ts
@@ -4,6 +4,7 @@ import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { AppRoutingModule } from "./app.routing";
 import { NativeScriptUISideDrawerModule } from "nativescript-telerik-ui/sidedrawer/angular";
 import { TNSFontIconModule } from 'nativescript-ngx-fonticon';
+import { NativeScriptUIListViewModule } from "nativescript-telerik-ui/listview/angular";
 
 import { AppComponent } from "./app.component";
 
@@ -13,6 +14,7 @@ import { DrawerComponent } from "./shared/drawer/drawer.component";
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 import { DishService } from './services/dish.service'; 
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
@@ -39,8 +41,8 @@ import { FavoriteService } from './services/favorite.service';
         NativeScriptUISideDrawerModule,
         TNSFontIconModule.forRoot({ 
             'fa': './fonts/font-awesome.min.css' 
-        })
-        
+        }),
+        NativeScriptUIListViewModule
     ],
     declarations: [
         AppComponent,
@@ -49,7 +51,8 @@ import { FavoriteService } from './services/favorite.service';
         DrawerComponent,
         HomeComponent,
         ContactComponent,
-        AboutComponent
+        AboutComponent,
+        FavoritesComponent        
     ],
     providers: [
         {provide: 'BaseURL', useValue: baseURL}, 
